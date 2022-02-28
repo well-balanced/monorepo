42b9487a743beef1b5704c14455888d7a8ff7530
well-balanced <iam.woosik.kim@gmail.com>
2022-02-28 12:21:01 +0900

    :bulb: Use shared package in server

--- lerna/packages/server/src/main.ts
+++ lerna/packages/server/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { printHello } from 'shared';
 
 async function bootstrap() {
+  printHello();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
